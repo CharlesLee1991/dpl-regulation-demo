72311442b0a4ea6af581962f2d610124bdab397f
CharlesLee1991 <lcseung@bizspring.co.kr>
2026-06-22 18:18:52 +0900

    feat: DBPoolListener FrontSetupExtension 호출

--- WEB-INF/src/db/DBPoolListener.java
+++ WEB-INF/src/db/DBPoolListener.java
@@ -3,6 +3,7 @@ package db;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.sql.Connection;
 
 @WebListener
 public class DBPoolListener implements ServletContextListener {
@@ -11,6 +12,12 @@ public class DBPoolListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
         DBPool.init(sce.getServletContext());
         System.out.println("[DPL] DBPool initialized");
+        // 프론트엔드 테이블 초기화
+        try (Connection conn = DBPool.getConnection()) {
+            FrontSetupExtension.init(conn);
+        } catch (Exception e) {
+            System.err.println("[DPL] FrontSetup 실패: " + e.getMessage());
+        }
     }
 
     @Override
